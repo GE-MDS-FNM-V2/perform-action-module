c0ce6b4cc69f80822f811779c2e27f3fe7ee7e86
MattThyng <mattthyng@gmail.com>
2020-03-28 10:45:40 -0400

    feat: added logging

--- src/clients/httpclient.ts
+++ src/clients/httpclient.ts
@@ -4,7 +4,7 @@ import { Client } from './client'
 import { transType } from '../enums/enums'
 import { ActionTypeV1, ActionObjectInformationV1 } from '@ge-fnm/action-object'
 import { HttpProtocol } from '../protocols/httpProtocol'
-
+import { pamLog } from '..'
 export class HttpClient implements Client {
   private axiosSession: AxiosInstance
   private config: AxiosRequestConfig = {}
@@ -15,7 +15,22 @@ export class HttpClient implements Client {
   private tokenPattern = /sessionid_80=.*==;/i
   private loggedin = false
 
+  /**
+   * Creates an httpclient object
+   * @param uri the ip address of the radio
+   * @param protocolstr the protocol to use, e.g. jsonrpc
+   * @param username the username to authenticate
+   * @param password the password to authenticate
+   */
   constructor(uri: string, protocolstr: string, username?: string, password?: string) {
+    pamLog(
+      'HTTPCLIENT:: Creating httpclient with uri: %s, protocol: %s, username: %s, password: %s',
+      uri,
+      protocolstr,
+      username,
+      password
+    )
+    // This needs to be changed to an if statement after more http protocols are added
     this.protocol = new Jsonrpc(username, password)
     this.uri = this.protocol.getURL(uri)
     this.config = this.protocol.config()
@@ -24,16 +39,28 @@ export class HttpClient implements Client {
     this.password = password
   }
 
+  /**
+   * Logs into the radio. Returns a promise with radio response
+   */
   login(): Promise<string> {
     return new Promise((resolve, reject) => {
       if (this.username !== undefined || this.password !== undefined) {
+        pamLog(
+          'HTTPCLIENT:: Logging into radio uri: %s, username: %s, password: %s',
+          this.uri,
+          this.username,
+          this.password
+        )
         let loginCmd = this.protocol.login()
+        pamLog('HTTPCLIENT:: Login Command:\n%O', loginCmd)
         this.axiosSession
           .post(this.uri, loginCmd)
           .then(response => {
+            pamLog('HTTPCLIENT:: Received the following login response: %O', response.data)
             let responseStr = JSON.stringify(response.data)
             /* istanbul ignore next */
             if (Object.keys(response.data.result).length === 0 && !('error' in response.data)) {
+              pamLog('HTTPCLIENT:: Login successful')
               this.loggedin = true
               let tokens = this.tokenPattern.exec(response.headers['set-cookie'])
               /* istanbul ignore next */
@@ -43,37 +70,50 @@ export class HttpClient implements Client {
               resolve(responseStr)
             } else {
               /* istanbul ignore next */
+              pamLog('HTTPCLIENT:: Login failed')
               resolve(`Login failed for ${this.uri} please check client data`)
             }
           })
           .catch(er => {
+            pamLog('HTTPCLIENT:: Login failed: %s', er)
             reject('ERROR: Unable to log in: ' + er)
           })
       } else {
         this.loggedin = true
+        pamLog('HTTPCLIENT:: Login not needed')
         resolve('No need to log in')
       }
     })
   }
 
+  /**
+   * Calls an GET or SET action on the radio
+   * Returns a promise with radio response
+   * @param action the action object information
+   */
   async call(action: ActionObjectInformationV1): Promise<string> {
     let transCmd = undefined
     let type: transType = transType.GET // default to GET
-
+    pamLog('HTTPCLIENT:: Received action obj: \n%s', action)
     if (this.loggedin === true) {
+      pamLog('HTTPCLIENT:: Client is logged in')
       if (action.actionType === ActionTypeV1.GET) {
+        pamLog('HTTPCLIENT:: Received GET action')
         transCmd = this.protocol.transaction(transType.GET)
       } else if (action.actionType === ActionTypeV1.SET) {
+        pamLog('HTTPCLIENT:: Received SET action')
         transCmd = this.protocol.transaction(transType.SET)
         type = transType.SET
       } else {
         throw new Error('Not a valid action type')
       }
-
+      pamLog('HTTPCLIENT:: Transaction Command:\n%O', transCmd)
       let transResponse = await this.axiosSession.post(this.uri, transCmd)
+      pamLog('HTTPCLIENT:: Received the following transaction response: %O', transResponse.data)
       this.protocol.setTrans(transResponse.data)
       let path: string[] | undefined = action.path
       if (path !== undefined) {
+        pamLog('HTTPCLIENT:: Setting path to %s', path.toString())
         this.protocol.setPath(path)
       } else {
         throw new Error('GET/SET commands need a path')
@@ -81,10 +121,12 @@ export class HttpClient implements Client {
 
       if (type === transType.GET) {
         let schemaCmd = this.protocol.getSchema()
+        pamLog('HTTPCLIENT:: Get schema Command:\n%O', schemaCmd)
         return new Promise<string>((resolve, reject) => {
           this.axiosSession
             .post(this.uri, schemaCmd)
             .then(actionResponse => {
+              //pamLog('HTTPCLIENT:: Received the following transaction response: %s', transResponse)
               let actionResponseStr = JSON.stringify(actionResponse.data)
               resolve(actionResponseStr)
             })
@@ -96,16 +138,23 @@ export class HttpClient implements Client {
       } else {
         // Else this is a set command...
         let setCmd = this.protocol.setValues(action.modifyingValue)
+        pamLog('HTTPCLIENT:: Set values Command:\n%O', setCmd)
         let validateCmd = this.protocol.validateCommit()
+        pamLog('HTTPCLIENT:: Validate commit Command:\n%O', validateCmd)
         let commitCmd = this.protocol.commit()
+        pamLog('HTTPCLIENT:: Commit Command:\n%O', commitCmd)
         let setResponse = await this.axiosSession.post(this.uri, setCmd)
-        console.log('Set response: ' + JSON.stringify(setResponse.data))
+        pamLog('HTTPCLIENT:: Received the following set values response:\n%O', setResponse.data)
         let validResponse = await this.axiosSession.post(this.uri, validateCmd)
-        console.log('Validate commit response: ' + JSON.stringify(validResponse.data))
+        pamLog('HTTPCLIENT:: Received the following set values response:\n%O', validResponse.data)
         return new Promise<string>((resolve, reject) => {
           this.axiosSession
             .post(this.uri, commitCmd)
             .then(commitResponse => {
+              pamLog(
+                'HTTPCLIENT:: Received the following commit response:\n%O',
+                commitResponse.data
+              )
               let actionResponseStr = JSON.stringify(commitResponse.data)
               resolve(actionResponseStr)
             })
@@ -120,11 +169,17 @@ export class HttpClient implements Client {
     }
   }
 
-  // For testing purposes ONLY
+  /**
+   * Sets status of Login. Recommended use only for testing
+   * @param status the status to set login to
+   */
   setLogin(status: boolean) {
     this.loggedin = status
   }
 
+  /**
+   * Returns current login status
+   */
   getLoginStatus(): boolean {
     if (this.loggedin === true) {
       return true
@@ -133,6 +188,9 @@ export class HttpClient implements Client {
     }
   }
 
+  /**
+   * Kills current client session. Returns a promise with radio response
+   */
   killsession(): Promise<boolean> {
     return new Promise((resolve, reject) => {
       let logout = this.protocol.logout()
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,23 @@ import { Client } from './clients/client'
 import { ClientType } from './enums/enums'
 import { v1, ActionTypeV1, ActionObjectInformationV1 } from '@ge-fnm/action-object'
 import { TSMap } from 'typescript-map'
+import { debug } from 'debug'
+
+export const pamLog = debug('ge-fnm:perform-action-module')
 
 export class Executer {
+  // Initated client objects are held in the MAP. URI is key
   private clientObjs = new TSMap<string, Client>()
 
+  /**
+   * Takes radio data and creates a client objext to the clientObjs map.
+   * Returns a promise of the login response from the radio.
+   * @param uri the ip address or serial port of radio
+   * @param type the client type, e.g. http, serial, etc...
+   * @param protocol the protocol to be used, e.g. JSONRPC, CLI
+   * @param username OPTIONAL the username to authenticate
+   * @param passoword OPTIONAL the password to authenticate
+   */
   addclient(
     uri: string,
     type: string,
@@ -14,6 +27,14 @@ export class Executer {
     username?: string,
     password?: string
   ): Promise<string> {
+    pamLog(
+      'Executer:: Adding client with uri: %s type: %s protocol: %s username: %s password: %s',
+      uri,
+      type,
+      protocol,
+      username,
+      password
+    )
     // remove this ignore later when wehave other client types
     /* istanbul ignore next */
     if (type === ClientType.HTTP) {
@@ -22,11 +43,19 @@ export class Executer {
     return this.clientObjs.get(uri).login()
   }
 
+  /**
+   * Takes a serialized action object and calls commands on designated client
+   * Returns serialized action object with response or error
+   * @param action serialized action object
+   */
   execute(action: string): Promise<string> {
+    pamLog('Executer:: Executing serialized action object:\n%s', action)
     return new Promise((resolve, reject) => {
       let actionObj = v1.deserialize(action)
       let actionData = actionObj.information
       let key: string = actionData.uri
+      pamLog('Executer:: Action Type: %s', actionObj.information.actionType)
+      pamLog('Executer:: Client URI: %s', key)
       if (actionObj.information.actionType === ActionTypeV1.INIT) {
         this.addclient(
           key,
@@ -47,19 +76,23 @@ export class Executer {
             // Either Axios error (connection refused), or Protocol is invalid
             /* istanbul ignore next */
             addClientError = addClientError.toString()
+            /* istanbul ignore next */
             actionObj.information.response = {
               error: `Error while adding client with uri ${key}. ${addClientError}`,
               data: null
             }
+            /* istanbul ignore next */
             reject(actionObj.serialize())
           })
       } else {
         if (this.clientObjs.has(key)) {
+          pamLog('Executer:: Found initiated client with uri: %s', key)
           this.clientObjs
             .get(key)
             .call(actionObj.information)
             .then(getSetResponse => {
               // Action succeeded
+              pamLog('Executer:: Received response from the radio: %s', getSetResponse)
               actionObj.information.response = {
                 data: getSetResponse,
                 error: null
@@ -68,6 +101,7 @@ export class Executer {
             })
             .catch(getSetError => {
               // Axios error (connection refused), invalid action type, Not logged in
+              pamLog('Executer:: Received the following ERROR: %s', getSetError)
               /* istanbul ignore next */
               actionObj.information.response = {
                 error: getSetError.toString(),
@@ -76,6 +110,7 @@ export class Executer {
               reject(actionObj.serialize())
             })
         } else {
+          pamLog('Executer:: No initiated client found with uri: %s', key)
           actionObj.information.response = {
             error: 'Not a valid radio uri. Please initialize radio before sending commands',
             data: null
@@ -86,20 +121,31 @@ export class Executer {
     })
   }
 
+  /**
+   * Kills the current session for the client
+   * Returns radio response
+   * @param uri the serial port or ip address of the client
+   */
   killClientSession(uri: string): Promise<boolean> {
+    pamLog('Executer:: Killing session for client with uri: %s', uri)
     return new Promise((resolve, rejects) => {
       if (this.clientObjs.has(uri)) {
+        pamLog('Executer:: Found initiated client with uri: %s', uri)
         this.clientObjs
           .get(uri)
           .killsession()
           .then(response => {
+            pamLog('Executer:: Kill session radio response: %s', response)
             resolve(response)
           })
           .catch(error => {
             /* istanbul ignore next */
+            pamLog('Executer:: ERROR killing session: %s', error)
+            /* istanbul ignore next */
             rejects(error)
           })
       }
+      pamLog('Executer:: No initiated client found with uri: %s', uri)
       rejects('No client session')
     })
   }
